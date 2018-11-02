bbad1b182542387e7c97d8fa2685839e6d19dd8b
Daniel <devidra87@gmail.com>
2018-11-02 13:08:02 -0700

    add back end code for pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
-var Order = {
-  this.pizzasize = size;
-  this.sauce = sauce;
-  this.cheese = cheese;
-  this.toppings = toppings;
-  this.amount = amount;
+function Order (pizzaSize, toppings) {
+  this.pizzaSize = pizzaSize;
+  this.sauce = 1;
+  this.cheese = 1;
+  this.toppings = toppings * 1.5;
+  this.price = 0
 }
+
+Order.prototype.finalCost = function (){
+  if(this.pizzaSize == "small"){
+    this.price += 5;
+  } else if (this.pizzaSize == "medium"){
+    this.price += 8;
+  } else if (this.pizzaSize == "large") {
+  this.price += 10
+  }
+  this.price += this.sauce;
+  this.price += this.cheese;
+  this.price += this.toppings;
+  return this.price
+}
+
+
+
+
+var pizzaSize = "small"
+var toppings = 3
+
+var newPizza = new Order (pizzaSize, toppings);
+
+
+
+
+
+newPizza.finalCost();
+
+console.log(newPizza.price)
